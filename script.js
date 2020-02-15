61bc3cbbc78ee3aeb52588e49629a0717a4db663
VyLe <thaovyletruong@gmail.com>
2020-02-15 11:27:20 -0600

    update reponsiveness of work section

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const greatreads = {
 /*---------------------
      Projects Display
 ----------------------*/
-const projects = [greatreads, tessa, detective]
+const projects = [tessa, greatreads, detective]
 const titleHolder = document.querySelectorAll('#work .work .pic')
 const popUp = document.querySelector('#pop-up')
 
