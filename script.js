199734fec75d6321cd9edf62d243f842f8e2685b
Vy Le <50475727+viviRbi@users.noreply.github.com>
2024-02-12 14:19:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,8 @@ const greatreads = {
 /*---------------------
      Projects Display
 ----------------------*/
-const projects = [tessa, detective]
+// const projects = [tessa, greatreads, detective]
+const projects = [detective]
 const titleHolder = document.querySelectorAll('#work .work .pic')
 const popUp = document.querySelector('#pop-up')
 
